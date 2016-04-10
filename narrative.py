5a92143206851a2c0253b096799b3279f68e9024
Tony Francis <tonyfrancisv@gmail.com>
2016-04-09 23:53:34 -0400

    Without gevent

--- narrative.py
+++ narrative.py
@@ -29,4 +29,5 @@ def user_spoke():
     return chatbot.get_response(text)['text']
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 33507))
+    app.run(host='0.0.0.0', port=port)
