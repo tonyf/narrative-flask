ea510a56066624232685d23123ee83cb3325d13d
Tony Francis <tonyfrancisv@gmail.com>
2016-04-24 22:29:39 -0400

    Refining lightbox

--- static/js/page.js
+++ static/js/page.js
@@ -4,8 +4,13 @@ function updateScroll(){
 }
 
 function showVideo(video) {
-    $('body').append('<div style="visibility:hidden"><div class="video-lightbox-container ' + video + '"><video class="lightbox-video" width="700" height="400" autoplay><source src="http://s3.amazonaws.com/lola-videos/intro.mov" type="video/mp4"></video></div></div>');
+    $('body').append('<div style="visibility:hidden"><div class="video-lightbox-container ' + video + '"><video id="' + video + '"class="lightbox-video" width="700" height="400" autoplay><source src="http://s3.amazonaws.com/lola-videos/intro.mov" type="video/mp4"></video></div></div>');
     $.featherlight($("." + video));
+
+    document.getElementById(video).addEventListener('ended',myHandler,false);
+    function myHandler(e) {
+        closeVideo();
+    }
 }
 
 function closeSplash() {
@@ -15,4 +20,9 @@ function closeSplash() {
 function closeIntro() {
     $('#intro').fadeOut(2000);
     chatbotSays("Hi, Lola. What are you going to do?");
+}
+
+function closeVideo() {
+    var current = $.featherlight.current();
+    current.close();
 }
