06a50e3dfeb6cd7d12040ff8ba4d1490e03ae781
Tony Francis <tonyfrancisv@gmail.com>
2016-04-24 22:03:40 -0400

    Video lightbox

--- static/js/page.js
+++ static/js/page.js
@@ -4,8 +4,8 @@ function updateScroll(){
 }
 
 function showVideo(video) {
-
-
+    $('body').append('<div class="video-lightbox-container"><video class="lightbox-video" width="1000" height="1000" autoplay><source src="http://s3.amazonaws.com/lola-videos/intro.mov" type="video/mp4"></video></div>');
+    $.featherlight($('.video-lightbox-container'), configuration);
 }
 
 function closeSplash() {
