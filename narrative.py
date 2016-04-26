878b36f6e4e423de063ba55d6fed8dac216c3f80
Tony Francis <tonyfrancisv@gmail.com>
2016-04-26 13:31:24 -0400

    Reset for demo

--- narrative.py
+++ narrative.py
@@ -1,9 +1,24 @@
 from flask import Flask, request, jsonify, render_template, json
-from chatbot import Chatbot
+from chatterbot import ChatBot
 import sys
 import os
 
 app = Flask(__name__, static_url_path='')
+
+chatbot = ChatBot(
+    'Amy',
+    io_adapter="chatterbot.adapters.io.JsonAdapter"
+)
+chatbot.train("chatterbot.corpus.english")
+chatbot.train([
+    "Hi",
+    "Hello, how are you feeling today?",
+    "I'm alright.",
+    "Do you want to talk about what happened?",
+    "Sure",
+    "What do you remember?",
+    "I was in the back of a car",
+])
 
 @app.route('/')
 def index():
@@ -12,7 +27,7 @@ def index():
 @app.route('/api/chatbot/', methods=['POST'])
 def user_spoke():
     text = request.form['text']
-    return chatbot.get_response(text)
+    return chatbot.get_response(text)['text']
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 33507))
